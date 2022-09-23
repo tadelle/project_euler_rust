0e370eea3f4447f6c4a070e4c1b2c00c639427c0
Jose Carlos Tadelle Jr <jose.tadelle@gmail.com>
2022-09-22 22:37:22 -0300

    Usando arredondamento no problema 29.

--- src/problems/problem029.rs
+++ src/problems/problem029.rs
@@ -26,7 +26,8 @@ fn get_result_problem() -> i64 {
 
     for a in 2..101 {
         for b in 2..101 {
-            let result = (a as f64).powf(b as f64);
+            let result = (a as f64).powf(b as f64).round();
+            
             map.insert(result.to_string(), result);
         }
     }
